42d7e64fbd43a5d863331a8816194f753956b270
eLLabTeacher <eLLabTeacher@DESKTOP-S03VIGT>
2024-05-28 19:43:48 +0200

    Vezbe 9

---
+++ InitialProject/src/gui/FrmTest.java
@@ -0,0 +1,166 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JToggleButton;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JLabel;
+import javax.swing.JScrollBar;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.AbstractListModel;
+import javax.swing.DefaultListModel;
+import javax.swing.ButtonGroup;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+
+public class FrmTest extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private DefaultListModel dlm = new DefaultListModel();
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmTest frame = new FrmTest();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmTest() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setTitle("OOIT grupa 4");
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel pnlCenter = new JPanel();
+		contentPane.add(pnlCenter, BorderLayout.CENTER);
+		GridBagLayout gbl_pnlCenter = new GridBagLayout();
+		gbl_pnlCenter.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_pnlCenter.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_pnlCenter.columnWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_pnlCenter.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		pnlCenter.setLayout(gbl_pnlCenter);
+		
+		JToggleButton tglbtnCrvena = new JToggleButton("Crvena");
+		tglbtnCrvena.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				dlm.addElement(tglbtnCrvena.getText());
+			}
+		});
+		buttonGroup.add(tglbtnCrvena);
+		GridBagConstraints gbc_tglbtnCrvena = new GridBagConstraints();
+		gbc_tglbtnCrvena.insets = new Insets(0, 0, 5, 5);
+		gbc_tglbtnCrvena.gridx = 0;
+		gbc_tglbtnCrvena.gridy = 0;
+		pnlCenter.add(tglbtnCrvena, gbc_tglbtnCrvena);
+		
+		JLabel lblCrvena = new JLabel("Crvena");
+		GridBagConstraints gbc_lblCrvena = new GridBagConstraints();
+		gbc_lblCrvena.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCrvena.gridx = 1;
+		gbc_lblCrvena.gridy = 0;
+		pnlCenter.add(lblCrvena, gbc_lblCrvena);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 2;
+		gbc_scrollPane.gridy = 0;
+		pnlCenter.add(scrollPane, gbc_scrollPane);
+		
+		JList lstBoje = new JList();
+		
+		lstBoje.setModel(dlm);
+		scrollPane.setViewportView(lstBoje);
+		
+		JToggleButton tglbtnPlava = new JToggleButton("Plava");
+		tglbtnPlava.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dlm.addElement(tglbtnPlava.getText());
+			}
+		});
+		buttonGroup.add(tglbtnPlava);
+		GridBagConstraints gbc_tglbtnPlava = new GridBagConstraints();
+		gbc_tglbtnPlava.insets = new Insets(0, 0, 5, 5);
+		gbc_tglbtnPlava.gridx = 0;
+		gbc_tglbtnPlava.gridy = 1;
+		pnlCenter.add(tglbtnPlava, gbc_tglbtnPlava);
+		
+		JLabel lblPlava = new JLabel("Plava");
+		GridBagConstraints gbc_lblPlava = new GridBagConstraints();
+		gbc_lblPlava.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPlava.gridx = 1;
+		gbc_lblPlava.gridy = 1;
+		pnlCenter.add(lblPlava, gbc_lblPlava);
+		
+		JToggleButton tglbtnZuta = new JToggleButton("Zuta");
+		tglbtnZuta.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				dlm.addElement(tglbtnZuta.getText());
+			}
+		});
+		buttonGroup.add(tglbtnZuta);
+		GridBagConstraints gbc_tglbtnZuta = new GridBagConstraints();
+		gbc_tglbtnZuta.insets = new Insets(0, 0, 5, 5);
+		gbc_tglbtnZuta.gridx = 0;
+		gbc_tglbtnZuta.gridy = 2;
+		pnlCenter.add(tglbtnZuta, gbc_tglbtnZuta);
+		
+		JLabel lblZuta = new JLabel("Zuta");
+		GridBagConstraints gbc_lblZuta = new GridBagConstraints();
+		gbc_lblZuta.insets = new Insets(0, 0, 5, 5);
+		gbc_lblZuta.gridx = 1;
+		gbc_lblZuta.gridy = 2;
+		pnlCenter.add(lblZuta, gbc_lblZuta);
+		
+		JPanel pnlSouth = new JPanel();
+		contentPane.add(pnlSouth, BorderLayout.SOUTH);
+		
+		JButton btnIspisi = new JButton("Antistres dugme :-)");
+		btnIspisi.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JOptionPane.showMessageDialog(null, "Antistres dugme",
+						"Poruka", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+		pnlSouth.add(btnIspisi);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.NORTH);
+		
+		
+		
+	}
+
+}
